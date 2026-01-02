44cc19cf7a5bed0e113102caf7c076c202c5d123
abigailflorence97 <abigail.smith@hmcts.net>
2026-01-02 12:58:00 +0000

    fix test

--- src/azure-sa/test/app.test.js
+++ src/azure-sa/test/app.test.js
@@ -1,12 +1,19 @@
 const request = require('supertest');
-const { expect } = require('chai');
 const app = require('../index');
 
 describe('GET /files', function() {
+  let expect;
+
+  before(async () => {
+    const chai = await import('chai');
+    expect = chai.expect;
+  });
+
   it('should return a list of files', function(done) {
     request(app)
       .get('/files')
       .end((err, res) => {
+        if (err) return done(err);
         expect(res.statusCode).to.equal(200);
         expect(res.body).to.be.an('array');
         done();
