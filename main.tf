a8c96da7d21aa2d0dec54b747ceddf34f385cf95
abigailflorence97 <abigail.smith@hmcts.net>
2026-01-06 12:16:24 +0000

    update tf pipeline

--- main.tf
+++ main.tf
@@ -8,10 +8,9 @@ provider "azurerm" {
     }
   }
 
-  subscription_id = var.subscription_id
-  tenant_id       = var.tenant_id
-  client_id       = var.client_id
-  client_secret   = var.client_secret
+  use_oidc = true
+  subscription_id = "48740eb3-ae90-47df-9a7b-b7833ad9314e"
+  tenant_id       = "34cc7ab5-fd16-446f-bd35-2d04775d2de1"
 }
 
 data "azurerm_client_config" "current" {}
