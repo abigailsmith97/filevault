43312964209a9057e5346bcbfe96a1b3413f89c9
abigailflorence97 <abigail.smith@hmcts.net>
2026-01-06 12:53:32 +0000

    update helm release

--- grafana-k8s-monitoring.tf
+++ grafana-k8s-monitoring.tf
@@ -26,8 +26,7 @@ resource "helm_release" "grafana-k8s-monitoring" {
 
   set_sensitive {
     name  = "destinations[0].auth.password"
-    # REMOVED base64decode: data source handles this automatically
-    value = data.kubernetes_secret_v1.firevault_k8s_secret.data["DESTINATIONS_PROMETHEUS_PASSWORD"]
+    value = kubernetes_secret_v1.firevault_k8s_secret.data["DESTINATIONS_PROMETHEUS_PASSWORD"]
   }
 
   set {
@@ -42,8 +41,7 @@ resource "helm_release" "grafana-k8s-monitoring" {
 
   set_sensitive {
     name  = "destinations[1].auth.password"
-    # REMOVED base64decode
-    value = data.kubernetes_secret_v1.firevault_k8s_secret.data["DESTINATIONS_LOKI_PASSWORD"]
+    value = kubernetes_secret_v1.firevault_k8s_secret.data["DESTINATIONS_LOKI_PASSWORD"]
   }
 
   set {
@@ -58,8 +56,7 @@ resource "helm_release" "grafana-k8s-monitoring" {
 
   set_sensitive {
     name  = "destinations[2].auth.password"
-    # REMOVED base64decode
-    value = data.kubernetes_secret_v1.firevault_k8s_secret.data["DESTINATIONS_OTLP_PASSWORD"]
+    value = kubernetes_secret_v1.firevault_k8s_secret.data["DESTINATIONS_OTLP_PASSWORD"]
   }
 
   set {
@@ -84,8 +81,7 @@ resource "helm_release" "grafana-k8s-monitoring" {
 
   set_sensitive {
     name  = "alloy-metrics.remoteConfig.auth.password"
-    # REMOVED base64decode
-    value = data.kubernetes_secret_v1.firevault_k8s_secret.data["FLEETMANAGEMENT_PASSWORD"]
+    value = kubernetes_secret_v1.firevault_k8s_secret.data["FLEETMANAGEMENT_PASSWORD"]
   }
 
   set {
@@ -100,8 +96,7 @@ resource "helm_release" "grafana-k8s-monitoring" {
 
   set_sensitive {
     name  = "alloy-singleton.remoteConfig.auth.password"
-    # REMOVED base64decode
-    value = data.kubernetes_secret_v1.firevault_k8s_secret.data["FLEETMANAGEMENT_PASSWORD"]
+    value = kubernetes_secret_v1.firevault_k8s_secret.data["FLEETMANAGEMENT_PASSWORD"]
   }
 
   set {
@@ -116,8 +111,7 @@ resource "helm_release" "grafana-k8s-monitoring" {
 
   set_sensitive {
     name  = "alloy-logs.remoteConfig.auth.password"
-    # REMOVED base64decode
-    value = data.kubernetes_secret_v1.firevault_k8s_secret.data["FLEETMANAGEMENT_PASSWORD"]
+    value = kubernetes_secret_v1.firevault_k8s_secret.data["FLEETMANAGEMENT_PASSWORD"]
   }
 
   set {
@@ -132,6 +126,6 @@ resource "helm_release" "grafana-k8s-monitoring" {
 
   set_sensitive {
     name  = "alloy-receiver.remoteConfig.auth.password"
-    value = data.kubernetes_secret_v1.firevault_k8s_secret.data["FLEETMANAGEMENT_PASSWORD"]
+    value = kubernetes_secret_v1.firevault_k8s_secret.data["FLEETMANAGEMENT_PASSWORD"]
   }
 }
