35d2958088f1d03ae7b8d9f1fb803a37ed1c3c61
abigailflorence97 <abigail.smith@hmcts.net>
2026-01-06 14:25:08 +0000

    fix syntax in tf file

--- grafana-k8s-monitoring.tf
+++ grafana-k8s-monitoring.tf
@@ -3,7 +3,7 @@
 # ==============================================================================
 
 provider "helm" {
-  kubernetes {
+  kubernetes = {
     host                   = azurerm_kubernetes_cluster.aks.kube_admin_config[0].host
     client_certificate     = base64decode(azurerm_kubernetes_cluster.aks.kube_admin_config[0].client_certificate)
     client_key             = base64decode(azurerm_kubernetes_cluster.aks.kube_admin_config[0].client_key)
